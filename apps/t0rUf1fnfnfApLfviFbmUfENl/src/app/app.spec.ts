b9ce3c2c49f45d3414d29f1912a8bdd076c97f04
Karthikeyan somasundaram <karthikitmsp@gmail.com>
2025-11-20 09:40:43 +0000

    Remove NxWelcome import from app.spec.ts

--- apps/t0rUf1fnfnfApLfviFbmUfENl/src/app/app.spec.ts
+++ apps/t0rUf1fnfnfApLfviFbmUfENl/src/app/app.spec.ts
@@ -4,7 +4,7 @@ import { App } from './app';
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
     }).compileComponents();
   });
 
